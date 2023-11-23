fd241bd0502b0c51d2ec528540467bdc5c613f68
Nikhil G <nikhilspyder@gmail.com>
2023-11-23 13:15:39 -0600

    Committing ECommerce Application Impl

--- src/test/java/com/project/ecommerceapplication/TestConfig.java
+++ src/test/java/com/project/ecommerceapplication/TestConfig.java
@@ -5,17 +5,28 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
+import com.project.ecommerceapplication.service.CustomerService;
+import com.project.ecommerceapplication.service.OrderService;
 import com.project.ecommerceapplication.service.ProductService;
 
 @TestConfiguration
 public class TestConfig {
 
-    // If you have any additional beans or configurations for testing, define them here.
-
-    // Example: Creating a mock bean for ProductService
     @Bean
-    @Primary // This annotation is used to indicate that this bean should take precedence in case of conflicts.
+    @Primary
     public ProductService productService() {
         return Mockito.mock(ProductService.class);
+    }
+    
+    @Bean
+    @Primary
+    public OrderService orderService() {
+        return Mockito.mock(OrderService.class);
+    }
+    
+    @Bean
+    @Primary
+    public CustomerService customerService() {
+        return Mockito.mock(CustomerService.class);
     }
 }
